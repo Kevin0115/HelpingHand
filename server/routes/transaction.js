14635734de3fc775bf2b3dabf8ed44eb9ccd102c
Dooyong Pan <fairbattle@hotmail.com>
2019-11-30 12:23:09 -0800

    Test1

--- server/routes/transaction.js
+++ server/routes/transaction.js
@@ -3,6 +3,6 @@ var router = express.Router();
 
 var transaction_controller = require('../controllers/transaction_controller');
 
-
+router.get('/', transaction_controller.get_all_transactions);
 
 module.exports = router;
