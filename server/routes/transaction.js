9157717caac081f5695a5e4fc3405a335696b67a
Gary <garyh1998@gmail.com>
2019-11-30 12:20:47 -0800

    implemented transaction controller in route post ONLY

--- server/routes/transaction.js
+++ server/routes/transaction.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 var transaction_controller = require('../controllers/transaction_controller');
 
+router.post(`/`, transaction_controller.create_transaction);
+
 
 
 module.exports = router;
