223d1b0033fd09afdb895a47162b097da2f69ca1
Gary <garyh1998@gmail.com>
2019-11-30 13:29:09 -0800

    merchant values inserted

--- server/hh.sql
+++ server/hh.sql
@@ -34,8 +34,29 @@ create table transaction (
   foreign key (mid) references merchant
 );
 
-insert into receiver values (1, 'Kevin', 1234);
-insert into receiver values (2, 'Edmund', 1234);
-insert into receiver values (3, 'Doon', 1234);
-insert into receiver values (4, 'Emma', 1234);
-insert into receiver values (5, 'Gary', 1234);
+insert into receiver values (999991, 'Kevin', 1234);
+insert into receiver values (999992, 'Edmund', 1234);
+insert into receiver values (999993, 'Doon', 1234);
+insert into receiver values (999994, 'Emma', 1234);
+insert into receiver values (999995, 'Gary', 1234);
+
+insert into merchant values (111111, `McDonalds`);
+insert into merchant values (111112, `Wendys`);
+insert into merchant values (111113, `Arbys`);
+insert into merchant values (111114, `Nandos`);
+insert into merchant values (111115, `Pizza Hut`);
+insert into merchant values (111116, `Gap`);
+insert into merchant values (111117, `Old Navy`);
+insert into merchant values (111118, `Aritzia`);
+insert into merchant values (111119, `The Bay`);
+insert into merchant values (111120, `Walmart`);
+insert into merchant values (111121, `London Drugs`);
+insert into merchant values (111122, `Superstore`);
+insert into merchant values (111123, `Target`);
+insert into merchant values (111124, `Shoppers Drug Mart`);
+insert into merchant values (111125, `Pharmasave`);
+insert into merchant values (111126, `Thrifty Foods`);
+insert into merchant values (111127, `Trader Joes`);
+insert into merchant values (111128, `Best Buy`);
+insert into merchant values (111129, `Future Shop`);
+insert into merchant values (111130, `Apple Store`);
