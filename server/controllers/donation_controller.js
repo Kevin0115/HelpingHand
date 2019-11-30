6321589fbc03f077cc901aee5f867e52c18165a5
edmundohh <1edmundoh@gmail.com>
2019-11-30 12:54:26 -0800

    Added create_donation endpoint

--- server/controllers/donation_controller.js
+++ server/controllers/donation_controller.js
@@ -1,1 +1,29 @@
 const connection = require('../config/db_config');
+
+exports.create_donation = async (req, res) => {
+    const did = req.body.did;
+    const amount = req.body.amount;
+    const rid = req.body.rid;
+    const ts = req.body.ts;
+  
+    query = {
+      text: `insert into donation
+              values ($1,$2,$3,$4)`,
+      values: [did, amount, rid, ts]
+    }
+  
+    try {
+      const query_result = await connection.query(query);
+      console.log(query_result);
+      res.send({
+        success: true,
+        content: 'Donation successfully added.'
+      });
+    } catch(err) {
+      console.error(err);
+      res.send({
+        success: false,
+        content: err.detail
+      });
+    }
+}
